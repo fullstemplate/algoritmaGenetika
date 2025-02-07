ce71a2badbd6a39b8a48fabbbc69eb138616e2f8
amat <amat@mail.com>
2025-02-07 18:34:19 +0700

    feat: input login (placeholder: login with gmail)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,8 +15,8 @@ export default function Login() {
       {/* <Image src={tunggu} alt="logo" width={400} height={400} className={style.img}/> */}
       <h1 className="text-4xl font-bold text-center text-white font-sans py-10">Login.</h1>
       <form action="submit" className="flex flex-col items-center justify-center px-12 gap-2 ">
-        <input type="username" name="username" placeholder="Username " className="px-6 py-1 font-sans rounded-md border-2 text-black" />
-        <input type="password" name="password" placeholder="********" className="px-6 py-1 font-sans rounded-md border-2 text-black" />
+        <input type="username" name="username" placeholder="Login with gmail" className="px-6 py-1 font-sans rounded-md border-2 text-black" />
+        <input type="password" name="password" placeholder="Click button gmail" className="px-6 py-1 font-sans rounded-md border-2 text-black" />
         <a href="/api/auth/signin"  id="login" className={buttonVariants({ variant: "destructive" })}>Gmail</a> 
       </form>
     </div>
